50c8aa47378fe48601b7bc3242539e56498575cb
Pranshu Patel <70943732+pranshu05@users.noreply.github.com>
2022-03-05 10:53:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,9 @@
         }
       }, 0)
   }());
+window.onload = function(){
+  var anchors = document.getElementsByTagName('a');
+  for (var i=0; i<anchors.length; i++){
+    anchors[i].setAttribute('target', '_blank');
+  }
+}
